1f1a450ccc93ccaf291c305329c899787360f91d
Your Name <you@example.com>
2023-03-17 20:48:28 -0400

    Add

--- app.js
+++ app.js
@@ -5,16 +5,19 @@ loginForm.addEventListener('submit', (event) => {
   event.preventDefault(); // Prevent the default form submission behavior
   
   // Get the values of the email and password fields
-  const email = loginForm.email.value;
+  // const email = loginForm.email.value;
+  const username = loginForm.username.value;
   const password = loginForm.password.value;
-
+  
   // Check if the email and password are correct (this is just an example)
   if(checkbox.checked){
-  if (email === 'admin@email.com' && password === 'password') {
+  if (username === 'admin' && password === 'password') {
     // Redirect the user to the admin page
     window.location.href = 'min_page.html';
   } else {
     // Redirect the user to the main page
-    window.location.href = 'index.html' + '?username=' + email;
+    // localStorage.setItem('userEmail', email);
+    localStorage.setItem('userName', username);
+    window.location.href = 'index.html';
   }
 }});
--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-const hamburger = document.querySelector('.hamburger');
 const navigation = document.querySelector('.navigation');
-
-hamburger.addEventListener('click', () => {
-  navigation.classList.toggle('open');
-});
 
 const urlParams = new URLSearchParams(window.location.search);
 const username = urlParams.get('username');
 
 document.getElementById('username').textContent = username;
 
+const toggleButton = document.getElementById('dark-mode-toggle');
+		const body = document.body;
+
+		toggleButton.addEventListener('click', () => {
+			body.classList.toggle('dark-mode');
+		});
